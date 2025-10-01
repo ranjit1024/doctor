7a74526964cb197076852a2d7b7fc86fb3da6e98
ranjit <ranjitdas2048@gmail.com>
2025-10-01 11:07:36 +0530

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const monaSans = Mona_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],  // Specify the weights you want
+  display: 'swap',
+  variable: '--font-mona-sans'
 });
 
 export const metadata: Metadata = {
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${monaSans.className}`}
       >
         {children}
       </body>

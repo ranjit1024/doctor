71a7a692caa88b473d1f98b127c118d879cfb1e9
ranjit <ranjitdas2048@gmail.com>
2025-10-03 22:46:02 +0530

    shedule now

--- components/ui/banner.tsx
+++ components/ui/banner.tsx
@@ -1,5 +1,10 @@
 // AppointmentBanner.jsx
+"use client"
+
+import { useRouter } from "next/navigation";
+
 export default function AppointmentBanner() {
+  const router = useRouter()
   return (
     <div className="w-full 0 py-12 flex justify-center">
       <div className="bg-green-50 rounded-xl  py-14 w-full  text-center">
@@ -9,7 +14,9 @@ export default function AppointmentBanner() {
         <p className="text-gray-500 text-md mb-8">
           Fast, simple, and secure. Choose your doctor, select a time, and confirm your visit online.
         </p>
-        <button className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded-md font-medium  ">
+        <button onClick={()=>{
+          router.push('/auth/signin')
+        }} className="bg-indigo-600 hover:cursor-pointer hover:bg-indigo-700 text-white px-6 py-2 rounded-md font-medium  ">
           Schedule Now
         </button>
       </div>
